69d3b230d8602665e6b2d90ce8fe9e91d25ea458
Jim Walsh <jpwalsh1@gmail.com>
2014-03-07 17:55:20 -0500

    Updated to check all Acts in Normal

--- d3.py
+++ d3.py
@@ -4,35 +4,44 @@ import urllib2, json
 
 bnet = raw_input('Enter your Bnet ID, ex. Name#1234: ')
 bnet = bnet.replace('#', '-')
- 
-def getChars (bnet):
-	url = 'http://us.battle.net/api/d3/profile/' + bnet + '/'
-        
-	response = urllib2.urlopen(url)
-	json_profile = json.load(response)
-	
-	characters = []
-	for char in json_profile["heroes"]:
-		characters.append(char['id'])
-		 
-	return characters
- 
- 
-def checkQuests (characters):
-	act1 = ['The Fallen Star', 'The Legacy of Cain', 'A Shattered Crown', 'Reign of the Black King', 'Sword of the Stranger', 'The Broken Blade', 'The Doom in Wortham', 'Trailing the Coven', 'The Imprisoned Angel', 'Return to New Tristram']
+acts = ['act1', 'act2', 'act3', 'act4']
 
-	for character in characters:
-		url = 'http://us.battle.net/api/d3/profile/' + bnet + '/hero/' + str(character)
- 		response = urllib2.urlopen(url)
-		json_char = json.load(response)
-		
-		if len(json_char['progress']['normal']['act1']['completedQuests']) < 10 and json_char['level'] == 60:
-			print '{0} - {1} - Level {2}'.format(json_char['name'], json_char['class'], json_char['level'])
-			completed = []
-			for quest in json_char['progress']['normal']['act1']['completedQuests']:
-				completed.append(str(quest['name']))
-			
-			missing = set(act1) - set(completed)
-			for q in missing: print q	
- 
+
+act1 = ['The Fallen Star', 'The Legacy of Cain', 'A Shattered Crown', 'Reign of the Black King', 'Sword of the Stranger', 'The Broken Blade', 'The Doom in Wortham', 'Trailing the Coven', 'The Imprisoned Angel', 'Return to New Tristram']
+act2 = ['Shadows in the Desert', 'The Road to Alcarnus', 'City of Blood', 'A Royal Audience', 'Unexpected Allies', 'Betrayer of the Horadrim', 'Blood and Sand', 'The Black Soulstone', 'The Scouring of Caldeum', 'Lord of Lies']
+act3 = ['The Siege of Bastion\'s Keep', 'Turning the Tide', 'The Breached Keep', 'Tremors in the Stone', 'Machines of War', 'Siegebreaker', 'Heart of Sin']
+act4 = ['Fall of the High Heavens', 'The Light of Hope', 'Beneath the Spire', 'Prime Evil']
+
+def getChars (bnet):
+        url = 'http://us.battle.net/api/d3/profile/' + bnet + '/'
+
+        response = urllib2.urlopen(url)
+        json_profile = json.load(response)
+
+        characters = []
+        for char in json_profile["heroes"]:
+                characters.append(char['id'])
+
+        return characters
+
+
+def checkQuests (characters):
+
+        for character in characters:
+                url = 'http://us.battle.net/api/d3/profile/' + bnet + '/hero/' + str(character)
+                response = urllib2.urlopen(url)
+                json_char = json.load(response)
+
+                for act in acts:
+                        completed = []
+                        exec('total_quests = len(' + str(act) + ')')
+                        if len(json_char['progress']['normal'][act]['completedQuests']) < total_quests and json_char['level'] == 60:
+                                print '{0} - {1} - Level {2} - {3}'.format(json_char['name'], json_char['class'], json_char['level'], act)
+                                for quest in json_char['progress']['normal'][act]['completedQuests']:
+                                        completed.append(str(quest['name']))
+
+                                exec('missing = set(' + str(act) + ') - set(completed)')
+                                for q in missing:
+                                        print q
+
 checkQuests(getChars(bnet))
