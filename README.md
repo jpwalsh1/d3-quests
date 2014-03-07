cac8c7f9f5274cc755b6c22a67bf1870105337eb
Jim Walsh <jpwalsh1@gmail.com>
2014-03-07 12:00:04 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+d3-quests
+=========
+
+Python to check what quests in Act 1 your level 60 characters have not completed.
+
+# Usage #
+    ./d3.py;
+
---
+++ d3.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+import urllib2, json
+
+bnet = raw_input('Enter your Bnet ID, ex. Name#1234: ')
+bnet = bnet.replace('#', '-')
+ 
+def getChars (bnet):
+	url = 'http://us.battle.net/api/d3/profile/' + bnet + '/'
+        
+	response = urllib2.urlopen(url)
+	json_profile = json.load(response)
+	
+	characters = []
+	for char in json_profile["heroes"]:
+        	#print '{0} - {1}'.format(char['name'], char['id'])
+		characters.append(char['id'])
+		 
+	return characters
+ 
+ 
+def checkQuests (characters):
+	act1 = ['The Fallen Star', 'The Legacy of Cain', 'A Shattered Crown', 'Reign of the Black King', 'Sword of the Stranger', 'The Broken Blade', 'The Doom in Wortham', 'Trailing the Coven', 'The Imprisoned Angel', 'Return to New Tristram']
+
+	for character in characters:
+		url = 'http://us.battle.net/api/d3/profile/' + bnet + '/hero/' + str(character)
+ 		response = urllib2.urlopen(url)
+		json_char = json.load(response)
+		
+		if len(json_char['progress']['normal']['act1']['completedQuests']) < 10 and json_char['level'] == 60:
+			print '{0} - {1} - Level {2}'.format(json_char['name'], json_char['class'], json_char['level'])
+			completed = []
+			for quest in json_char['progress']['normal']['act1']['completedQuests']:
+				completed.append(str(quest['name']))
+			
+			missing = set(act1) - set(completed)
+			for q in missing: print q	
+ 
+checkQuests(getChars(bnet))
