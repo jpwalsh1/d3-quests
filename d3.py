43a91f0b160c7ef8cd36eecec3caa20e202bba06
Jim Walsh <jpwalsh1@gmail.com>
2014-04-14 19:10:32 +0000

    Updating for new API, level 70 and Act 5 tracking.

--- d3.py
+++ d3.py
@@ -6,11 +6,12 @@ bnet = raw_input('Enter your Bnet ID, ex. Name#1234: ')
 region = raw_input('Enter your region. us/eu/kr: ')
 bnet = bnet.replace('#', '-')
 
-acts = ['act1', 'act2', 'act3', 'act4']
+acts = ['act1', 'act2', 'act3', 'act4', 'act5']
 act1 = ['The Fallen Star', 'The Legacy of Cain', 'A Shattered Crown', 'Reign of the Black King', 'Sword of the Stranger', 'The Broken Blade', 'The Doom in Wortham', 'Trailing the Coven', 'The Imprisoned Angel', 'Return to New Tristram']
 act2 = ['Shadows in the Desert', 'The Road to Alcarnus', 'City of Blood', 'A Royal Audience', 'Unexpected Allies', 'Betrayer of the Horadrim', 'Blood and Sand', 'The Black Soulstone', 'The Scouring of Caldeum', 'Lord of Lies']
 act3 = ['The Siege of Bastion\'s Keep', 'Turning the Tide', 'The Breached Keep', 'Tremors in the Stone', 'Machines of War', 'Siegebreaker', 'Heart of Sin']
 act4 = ['Fall of the High Heavens', 'The Light of Hope', 'Beneath the Spire', 'Prime Evil']
+act5 = ['The Fall of Westmarch', 'Souls of the Dead', 'The Harbinger', 'The Witch', 'The Pandemonium Gate', 'The Battlefields of Eternity', 'Breaching the Fortress', 'Angel of Death']
 
 def getChars (bnet):
         url = 'http://' + region + '.battle.net/api/d3/profile/' + bnet + '/'
@@ -35,9 +36,9 @@ def checkQuests (characters):
                 for act in acts:
                         completed = []
                         exec('total_quests = len(' + str(act) + ')')
-                        if len(json_char['progress']['normal'][act]['completedQuests']) < total_quests and json_char['level'] == 60:
+                        if len(json_char['progression'][act]['completedQuests']) < total_quests and json_char['level'] == 70:
                                 print '{0} - {1} - Level {2} - {3}'.format(json_char['name'], json_char['class'], json_char['level'], act)
-                                for quest in json_char['progress']['normal'][act]['completedQuests']:
+                                for quest in json_char['progression'][act]['completedQuests']:
                                         completed.append(str(quest['name']))
 
                                 exec('missing = set(' + str(act) + ') - set(completed)')
